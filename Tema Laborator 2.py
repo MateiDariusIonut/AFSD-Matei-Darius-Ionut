e3a01fbef22c65d5a73f337b6c2ef1583d4572a6
MateiDariusIonut <Matei.Mi.Darius@student.utcluj.ro>
2024-10-17 11:56:55 +0300

    Create Tema Laborator 2.py

---
+++ Tema Laborator 2.py
@@ -0,0 +1,17 @@
+articol = "Fie că optezi pentru o tartă crocantă sau o prăjitură pufoasă, strugurii adaugă un gust dulce-acrișor în orice desert."
+jumatate1 = articol[:len(articol)//2]
+jumatate2 = articol[len(articol)//2:]
+
+jumatate1 = jumatate1.upper()
+jumatate1 = jumatate1.strip()
+
+x = jumatate2[:1]
+x = x.upper()
+jumatate2 = x+jumatate2[1:]
+jumatate2 = jumatate2.replace(".","")
+jumatate2 = jumatate2.replace(",","")
+jumatate2 = jumatate2.replace("!","")
+jumatate2 = jumatate2.replace("?","")
+jumatate2 = jumatate2[::-1]
+print (jumatate1 + jumatate2)
+
