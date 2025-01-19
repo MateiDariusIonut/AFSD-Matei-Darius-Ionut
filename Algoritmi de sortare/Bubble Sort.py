808153164f9058c50aae61604885845d94c54cc6
MateiDariusIonut <Matei.Mi.Darius@student.utcluj.ro>
2025-01-19 20:10:56 +0200

    Pregatire

--- Algoritmi de sortare/Bubble Sort.py
+++ Algoritmi de sortare/Bubble Sort.py
@@ -1,3 +1,5 @@
+import math
+
 def bubble_sort(lista):
     sortat = True
     while sortat:
@@ -19,3 +21,41 @@ def optimized_bubble_sort(lista):
                 sortat = True
         n -= 1
     return lista
+
+def cautare_binara(lista, st, dr, x):
+    while st<=dr:
+        mij=st+(dr-st)//2
+        if x==lista[mij]:
+            return mij
+        elif x>lista[mij]:
+            return cautare_binara(lista,mij+1,dr,x)
+        else:
+            return cautare_binara(lista, st, mij-1, x)
+    return -1
+
+def cautare_exponentiala(lista,val):
+    n=len(lista)
+    if lista[0]==val:
+        return 0
+    i=1
+    while i<n and lista[i]<val:
+        i = i*2
+    return cautare_binara(lista, i//2, min(i,n-1),val)
+
+def cautare_salt(lista,val):
+    n=len(lista)
+    salt=int(math.sqrt(n))
+    for i in range(0,n,salt):
+        if lista[i]<val:
+            st=i
+        elif lista[i]==val:
+            return i
+        else:
+            break
+    for i in range(lista[st:st+salt]):
+        if lista[i]==val:
+            return i
+    return -1
+
+lista1=[1,2,4,7,9,11,13,17,21,24,27,29,33,37,41]
+print(cautare_exponentiala(lista1,17))
