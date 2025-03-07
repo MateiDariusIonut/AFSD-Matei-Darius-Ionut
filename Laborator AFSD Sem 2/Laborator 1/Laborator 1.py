a3403845a6bcc7a7e461ec98cf1c086303bce011
MateiDariusIonut <Matei.Mi.Darius@student.utcluj.ro>
2025-03-07 11:57:20 +0200

    Laborator 1

---
+++ Laborator AFSD Sem 2/Laborator 1/Laborator 1.py
@@ -0,0 +1,12 @@
+import csv
+from faker import Faker
+
+
+
+#def generate_cnp():
+
+
+#with open("cnp.csv",'w',newline='') as csvfile:
+#    fieldnames = ['cnp','nume']
+#    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+#    writer.writerows(data)
